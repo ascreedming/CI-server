b59199b897a4ed91405067293bb4a921fa4c57aa
Pihlqvist <fpih@kth.se>
2019-02-07 10:35:43 +0100

    Adding report output to integrationTest functions
    A new struct called `report` is created to hold
    * error message
    * error code
    
    This was added to all the functions in integration test to control the flow of program.

--- source/integrationTest.cpp
+++ source/integrationTest.cpp
@@ -13,6 +13,12 @@
 *
 */
 
+struct report
+{ 
+    int errorcode;          // 0 -> good, 1 > -> bad
+    std::string message;    // message for more info
+};
+
 // Performes a system call and returns the output as a string
 std::string exec(const char* cmd) {
     std::array<char, 128> buffer;
@@ -29,63 +35,75 @@ std::string exec(const char* cmd) {
 
 
 // Clone the current repo into a selected folder
-void cloning(std::string sourceFolder, std::string repo)
+report cloning(std::string sourceFolder, std::string repo)
 {
     std::string cmd = "git clone " + repo + " " + sourceFolder;
     std::string cmd_output = exec(cmd.c_str());
 
+    report rep;
     if (cmd_output.find("done.") != std::string::npos)
     {
-        std::cout << "Cloning done!\n";
+        rep.message = "Cloning done!\n";
+        rep.errorcode = 0;
     }
     else if (cmd_output.find("fatal:") != std::string::npos)
     {
-        std::cout << "Fatal\n";
+        rep.message = "Fatal\n";
+        rep.errorcode = 2;
     }
     else
     {
-        std::cerr << "Unknown Error in cloning\n";
+        rep.message = "Unknown Error in cloning\n";
+        rep.errorcode = 1;
     }
 
+    return rep;
 }
 
 // Checkout the given 'commit'
-void checkout(std::string sourceFolder, std::string commit)
+report checkout(std::string sourceFolder, std::string commit)
 {
     std::string cmd = "git --git-dir " + sourceFolder + "/.git checkout " + commit;
     std::string cmd_output = exec(cmd.c_str());
 
-
+    report rep;
     if (cmd_output.find("HEAD is now at") != std::string::npos)
     {
-        std::cout << "We could set HEAD to commit\n";
+        rep.message = "We could set HEAD to commit\n";
+        rep.errorcode = 1;
     }
     else
     {
         std::cerr << "Unknown Error in git checkout\n";
     }
+
+    return rep;
 }
 
 
 // Merge to the target branch and determine if it could be done
-void merge(std::string sourceFolder, std::string targetBranch)
+report merge(std::string sourceFolder, std::string targetBranch)
 {
     std::string cmd = "git --git-dir " + sourceFolder + "/.git merge " + targetBranch;
     std::string cmd_output = exec(cmd.c_str());
 
-
+    report rep;
     if (cmd_output.find("Already up to date.") != std::string::npos)
     {
-        std::cout << "Merge went fine\n";
+        rep.message = "Merge went fine\n";
+        rep.errorcode = 0;
     }
     else
     {
-        std::cerr << "Unkown error in merge\n";
+        rep.message = "Unkown error in merge\n";
+        rep.errorcode = 1;
     }
+
+    return rep;
 }
 
 // Determine if CMake could compile the code
-void compileCMake(std::string sourceFolder, std::string buildFolder)
+report compileCMake(std::string sourceFolder, std::string buildFolder)
 {
     // Create build folder
     std::string cmd = "mkdir " + buildFolder;
@@ -96,50 +114,61 @@ void compileCMake(std::string sourceFolder, std::string buildFolder)
     cmd_output = exec(cmd.c_str());
 
     //std::cerr << cmd_output; // USED IN DEBUGGING
-
+    report rep;
     if (cmd_output.find("Generating done") != std::string::npos)
     {
-        std::cout << "CMake compiled fine\n";
+        rep.message = "CMake compiled fine\n";
+        rep.errorcode = 0;
     }
     else 
     {
-        std::cerr << "CMake din't compile\n";
+        rep.message = "CMake din't compile\n";
+        rep.errorcode = 0;
     }
 
+    return rep;
 }
 
 // Compile with the make file cretade by CMake in prev step
-void compileMake(std::string buildFolder)
+report compileMake(std::string buildFolder)
 {
     std::string cmd = "make -C " + buildFolder;
     std::string cmd_output = exec(cmd.c_str());    
 
-
+    report rep;
     if (cmd_output.find("100%") != std::string::npos)
     {
-        std::cout << "Make compiled fine\n";
+        rep.message = "Make compiled fine\n";
+        rep.errorcode = 0;
     }
     else 
     {
-        std::cerr << "Make din't compile\n";
+        rep.message = "Make din't compile\n";
+        rep.errorcode = 1;
     }
+
+    return rep;
 }
 
 // Run unittests in build folder
-void runUnittest(std::string buildFolder)
+report runUnittest(std::string buildFolder)
 {
     std::string cmd = buildFolder + "/unittest"; 
     std::string cmd_output = exec(cmd.c_str());
 
-
+    report rep;
     if (cmd_output.find("PASSED") != std::string::npos)
     {
-        std::cout << "Passed all unittests\n";
+        rep.message = "Passed all unittests\n";
+        rep.errorcode = 0;
     }
     else 
     {
-        std::cerr << "Failed unittests\n";
+        rep.message = "Failed unittests\n";
+        rep.errorcode = 1;
     }
+    
+    return rep;
 }
 
 void integrationTest()
