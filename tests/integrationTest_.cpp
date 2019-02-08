9e85636a5bf0256938ab1d2554a7b5042b83dd30
Fredrik <fpih@kth.se>
2019-02-08 09:48:58 +0100

    Adding test case for error in integrationTest_cpp
    The test clones a false repo that should give an error, this works as expected

--- tests/integrationTest_.cpp
+++ tests/integrationTest_.cpp
@@ -20,4 +20,20 @@ TEST(runIntegrationTest, correctly)
     EXPECT_STREQ("Everthing is good.", r.message.c_str());
     EXPECT_EQ(0, r.errorcode);
 
+    system("rm ./wormhole.wh");
+}
+
+//report integrationTest(std::string commit, std::string repo, std::string targetBranch)
+TEST(runIntegrationTest, wrong_repo)
+{
+
+    std::string repo = "git@github.com:Pihlqvist/ci_test_WRONGID.git";
+    std::string commit = "6cc7e58d7cb0e4b7c06eee65f1f49b336050db55";
+    std::string targetBranch = "origin/assurance";
+
+    report r = integrationTest(commit, repo, targetBranch);
+
+    EXPECT_STREQ("Fatal error while cloning.", r.message.c_str());
+    EXPECT_EQ(1, r.errorcode);
+
 }
